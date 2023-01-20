11ea99ef896ecbecc8cbd1b2a0c0ee6619c277d8
KeenScout <me@robincopier.com>
2023-01-20 13:45:52 +0100

    Update test_ProcessingLibary.py

--- project 1/test_ProcessingLibary.py
+++ project 1/test_ProcessingLibary.py
@@ -4,7 +4,7 @@ import ProcessingLibary
 class TestWordFrequencyAnalyzer(unittest.TestCase):
     
     def test_calculate_highest_frequency(self):
-        result = ProcessingLibary.WordFrequencyAnalyzer.calculate_highest_frequency("hello hello how are you")
+        result = ProcessingLibary.WordFrequencyAnalyzer.calculate_highest_frequency(self, "hello hello how are you")
         self.assertEqual(result, [('hello', 2)])
 
 if __name__ == '__main__':
