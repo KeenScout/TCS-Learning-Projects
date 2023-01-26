258c6f220d6e3e8398ab9599cba87cb453f69ba3
KeenScout <me@robincopier.com>
2023-01-26 15:41:40 +0100

    created tests and working.

---
+++ project1_1/test_main.py
@@ -0,0 +1,17 @@
+import main
+
+
+def test_calculate_highest_frequency():
+    # most basic functionality test
+    assert main.calculate_highest_frequency("The sun shines over the lake") == [('the', 2)]
+
+
+def test_calculate_frequency_for_word():
+    # most basic functionality test
+    assert main.calculate_frequency_for_word("The sun shines over the lake", "the") == 2
+
+
+def test_calculate_most_frequent_n_words():
+    # most basic functionality test
+    assert main.calculate_most_frequent_n_words("the sun shines over the lake", 3) == \
+           [('the', 2), ('lake', 1), ('over', 1)]
