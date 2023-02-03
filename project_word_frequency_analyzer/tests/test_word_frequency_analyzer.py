8bb2d3839939146b850a48a4f57f39c2a20c0d3c
KeenScout <me@robincopier.com>
2023-02-03 12:54:37 +0100

    .

--- project_word_frequency_analyzer/tests/test_word_frequency_analyzer.py
+++ project_word_frequency_analyzer/tests/test_word_frequency_analyzer.py
@@ -1,22 +1,21 @@
+import pytest
+
 from project_word_frequency_analyzer.src import word_frequency_analyzer
 
 
-def test_calculate_highest_frequency():
+@pytest.mark.parametrize("test_input, expected", [("The sun shines over the lake", [('the', 2)]),
+                                                  ("ThE SuN sHiNeS oVeR ThE LaKE", [('the', 2)])])
+def test_calculate_highest_frequency(test_input, expected):
     # most basic functionality tests
-    assert word_frequency_analyzer.calculate_highest_frequency("The sun shines over the lake") == [('the', 2)]
-    assert word_frequency_analyzer.calculate_highest_frequency("tHe SuN SHInes over ThE lake") == [('the', 2)]
+    assert word_frequency_analyzer.calculate_highest_frequency(test_input) == expected
 
 
 def test_calculate_frequency_for_word():
     # most basic functionality tests
     assert word_frequency_analyzer.calculate_frequency_for_word("The sun shines over the lake", "the") == 2
-    assert word_frequency_analyzer.calculate_frequency_for_word("tHe SuN SHInes over ThE lake", "the") == 2
 
 
 def test_calculate_most_frequent_n_words():
     # most basic functionality tests
     assert word_frequency_analyzer.calculate_most_frequent_n_words("the sun shines over the lake", 3) == \
            [('the', 2), ('lake', 1), ('over', 1)]
-    assert word_frequency_analyzer.calculate_most_frequent_n_words("tHe SuN SHInes over ThE lake", 3) == \
-           [('the', 2), ('lake', 1), ('over', 1)]
-    
